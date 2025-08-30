f735e0bad2828e41f000bbc8fe927f07815771a7
ntmssdbrn <doombled@gmail.com>
2025-08-30 17:16:54 +0000

    тут я заигрался и зачем то сделал систему с тем, кто первый ходит хотя это нахуй не надо, и в целом здесь не продумано если будут опечатки, всё на веру, что правильно

--- main.go
+++ main.go
@@ -2,6 +2,59 @@ package main
 
 import "fmt"
 
+func start() (string, string) {
+	var (
+		Player1 string
+		Player2 string
+	)
+	fmt.Println("Insert your names")
+	fmt.Scanf("%s\n", &Player1)
+	fmt.Scanf("%s\n", &Player2)
+	fmt.Println("All right,", Player1, "and", Player2)
+	return Player1, Player2
+}
+
+func chooseWhoFirst(number1 string, number2 string) (string, string) {
+	fmt.Println("Who first:", number1, "or", number2, "?")
+	var firstMoveName string
+	var secondMoveName string
+	fmt.Scanf("%s\n", &firstMoveName) // Выбираем первое имя
+	fmt.Println("Good,", firstMoveName)
+	if firstMoveName == number1 { // Второе имя относительно выбранного первого
+		secondMoveName = number2
+	} else {
+		secondMoveName = number1
+	}
+	return firstMoveName, secondMoveName
+}
+
+func game() int {
+	var object1 string
+	var object2 string
+	fmt.Println("You can write: stone, paper or scissors")
+	fmt.Println("Move1")
+	fmt.Scanf("%s\n", &object1)
+	fmt.Println("Move2")
+	fmt.Scanf("%s\n", &object2)
+
+	if (object1 == "stone" && object2 == "scissors") || (object1 == "paper" && object2 == "stone") || (object1 == "scissors" && object2 == "paper") {
+		return 1
+	} else if (object1 == "scissors" && object2 == "stone") || (object1 == "stone" && object2 == "paper") || (object1 == "paper" && object2 == "scissors") {
+		return 2
+	} else {
+		return 3
+	}
+}
+
 func main() {
-	fmt.Println("first line")
+	name1, name2 := start() // Оставляем, чтобы начислять победы по именам
+	first, second := chooseWhoFirst(name1, name2)
+	numberScenaries := game()
+	if numberScenaries == 1 {
+		fmt.Println(first, ", win")
+	} else if numberScenaries == 2 {
+		fmt.Println(second, ", win")
+	} else {
+		fmt.Println("Draw")
+	}
 }

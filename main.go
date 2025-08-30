6ae7d39d16aae397874f2fc80e688e2578a6fdcc
ntmssdbrn <doombled@gmail.com>
2025-08-30 17:34:07 +0000

    без первого и второго, просто по факту кого первого написал, того и первый обЪект(ножницы, камень или бумага)

--- main.go
+++ main.go
@@ -14,47 +14,25 @@ func start() (string, string) {
 	return Player1, Player2
 }
 
-func chooseWhoFirst(number1 string, number2 string) (string, string) {
-	fmt.Println("Who first:", number1, "or", number2, "?")
-	var firstMoveName string
-	var secondMoveName string
-	fmt.Scanf("%s\n", &firstMoveName) // Выбираем первое имя
-	fmt.Println("Good,", firstMoveName)
-	if firstMoveName == number1 { // Второе имя относительно выбранного первого
-		secondMoveName = number2
-	} else {
-		secondMoveName = number1
-	}
-	return firstMoveName, secondMoveName
-}
-
-func game() int {
+func game(chel1 string, chel2 string) {
 	var object1 string
 	var object2 string
 	fmt.Println("You can write: stone, paper or scissors")
-	fmt.Println("Move1")
+	fmt.Println(chel1, "Throw")
 	fmt.Scanf("%s\n", &object1)
-	fmt.Println("Move2")
+	fmt.Println(chel2, "Throw")
 	fmt.Scanf("%s\n", &object2)
 
 	if (object1 == "stone" && object2 == "scissors") || (object1 == "paper" && object2 == "stone") || (object1 == "scissors" && object2 == "paper") {
-		return 1
+		fmt.Println(chel1, ",wins.")
 	} else if (object1 == "scissors" && object2 == "stone") || (object1 == "stone" && object2 == "paper") || (object1 == "paper" && object2 == "scissors") {
-		return 2
+		fmt.Println(chel2, ",wins.")
 	} else {
-		return 3
+		fmt.Println("Draw")
 	}
 }
 
 func main() {
 	name1, name2 := start() // Оставляем, чтобы начислять победы по именам
-	first, second := chooseWhoFirst(name1, name2)
-	numberScenaries := game()
-	if numberScenaries == 1 {
-		fmt.Println(first, ", win")
-	} else if numberScenaries == 2 {
-		fmt.Println(second, ", win")
-	} else {
-		fmt.Println("Draw")
-	}
+	game(name1, name2)
 }
